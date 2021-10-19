73da7e171acfcd192eb7539406895eaee6cee15d
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-18 23:53:27 -0300

    Changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,10 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
+
+app.get('/github', (request, response) => {
+  response.redirect(`htpps://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
 
 app.listen(4000, () => { console.log('listening on port 4000') });
