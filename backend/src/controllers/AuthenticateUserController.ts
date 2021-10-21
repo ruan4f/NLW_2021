88548e5b8f4442d9922b27b95952e5c5f53228dd
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-20 23:02:18 -0300

    Create AuthenticateUserController.ts

---
+++ backend/src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import { AuthenticateUserService } from "../services/AuthenticateUserService";
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response) {
+    const service = new AuthenticateUserService();
+    // service.execute();
+  }
+}
