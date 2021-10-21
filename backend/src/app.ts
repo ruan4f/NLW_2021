bded58117574c245078db8e08a602bc58ec3572b
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-20 22:55:47 -0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 
 app.get('/github', (request, response) => {
-  response.redirect(`htpps://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
 });
 
 app.listen(4000, () => { console.log('listening on port 4000') });
