7b63cd93eac53c0e3e9bb72381aac88dc68829b2
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-21 23:01:56 -0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,11 @@
 import "dotenv/config";
 import express from "express";
 
+import { router } from "./routes";
+
 const app = express();
+
+app.use(router);
 
 app.get('/github', (request, response) => {
   response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
