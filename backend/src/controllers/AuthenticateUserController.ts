670f0da15af10eb6d0b93bb94520d2538200952a
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-23 23:42:08 -0300

    Update AuthenticateUserController.ts

--- backend/src/controllers/AuthenticateUserController.ts
+++ backend/src/controllers/AuthenticateUserController.ts
@@ -5,9 +5,12 @@ class AuthenticateUserController {
   async handle(request: Request, response: Response) {
     const { code } = request.body;
     const service = new AuthenticateUserService();
-    const result = await service.execute(code);
-
-    return response.json(result);
+    try {
+      const result = await service.execute(code);
+      return response.json(result);
+    } catch (error) {
+      return response.json(error.message);
+    }
   }
 }
 
