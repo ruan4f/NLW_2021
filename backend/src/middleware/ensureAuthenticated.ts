c1f402f9df0530ab403693d8ee0b680fdcfb5b8c
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-24 21:00:58 -0300

    Create ensureAuthenticated.ts

---
+++ backend/src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) {
+    return response.status(401).json({ errorCode: "token.invalid" });
+  }
+
+  //Bearer token
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return response.status(401).json({ errorCode: "token.expired" });
+  }
+
+
+
+}
