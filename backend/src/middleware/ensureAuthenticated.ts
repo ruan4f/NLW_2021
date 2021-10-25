8d8d96e181661f2ad7c73a97d5a1af0a7c07b44c
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-24 21:28:27 -0300

    Config express

--- backend/src/middleware/ensureAuthenticated.ts
+++ backend/src/middleware/ensureAuthenticated.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
+interface IPayLoad {
+  sub: string
+}
+
 export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
   const authToken = request.headers.authorization;
 
@@ -13,7 +17,9 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
   const [, token] = authToken.split(" ");
 
   try {
-    const { sub } = verify(token, process.env.JWT_SECRET);
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayLoad;
+
+    request.user_id = sub;
   } catch (error) {
     return response.status(401).json({ errorCode: "token.expired" });
   }
