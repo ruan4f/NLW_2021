ca7f17fc92f221458606fbdda71f298c51f1e666
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-24 22:25:25 -0300

    New config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -33,4 +33,4 @@ app.get('/signin/callback', (request, response) => {
   return response.json(code);
 });
 
-app.listen(4000, () => { console.log('listening on port 4000') });
+export { serverHttp, io }
