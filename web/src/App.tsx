1dc604e66c87114881afed84080639c7f1fa5e59
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-29 22:54:20 -0300

    New CSS file

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
+import styles from './App.module.css';
+
 export function App() {
   return (
-    <main className="contentWrapper">
+    <main className={styles.contentWrapper}>
       <h1>Hello World!</h1>
     </main>
   )
