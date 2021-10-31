9f3642d0bc7084f981620fbde9377493320a4dc0
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-30 23:00:53 -0300

    Update Login

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -2,9 +2,9 @@ import styles from './styles.module.scss';
 
 export function LoginBox() {
   return (
-    <div className={styles.contentWrapper}>
+    <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithgithub}>
+      <a href="#" className={styles.signInWithGithub}>
         Entrar com Github
       </a>
     </ div>
