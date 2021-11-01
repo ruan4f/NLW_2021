094bde3c3c74f65df00f10201758c1abc3db764d
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-31 23:14:54 -0300

    Connect to backend

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,31 +1,44 @@
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import styles from './styles.module.scss';
-
 import logoImg from '../../assets/logo.svg';
-import { useEffect } from "react";
+
+type Message = {
+  id: string;
+  text: string
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
 
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
   useEffect(() => {
-    api.get('messages/last3').then(response => {
-      console.log(response.data);
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data);
     })
-  }, [])
+  }, []);
 
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
-            </div>
-            <span>Diego fernandes</span>
-
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   )
