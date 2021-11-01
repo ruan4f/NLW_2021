2b968e2074a54c0af0c250846826356a57e94bfe
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-10-31 21:53:08 -0300

    Update index.tsx

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,7 +1,20 @@
 import styles from './styles.module.scss';
 
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos!</p>
+          <div className={styles.messageUser}>
+
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
