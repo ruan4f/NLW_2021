4cb5d94d12335ede72dce19fa17b702c336f187e
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-11-01 14:29:26 -0300

    Update index.tsx

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,9 +1,32 @@
 import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
 import styles from './styles.module.scss';
+import { api } from "../../services/api";
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
 
 export function LoginBox() {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=c43d704e5e77680a4219`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@dowhile:token', token);
+
+    console.log(user);
+  }
 
   useEffect(() => {
     const url = window.location.href;
@@ -14,7 +37,7 @@ export function LoginBox() {
 
       window.history.pushState({}, '', urlWithoutCode);
 
-
+      signIn(githubCode);
     }
 
   }, []);
