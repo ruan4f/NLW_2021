541d8c3fd023650f3fbb5cc842cb5f1265cd3027
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-11-01 14:22:10 -0300

    Update index.tsx

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=c43d704e5e77680a4219`;
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=');
+
+      window.history.pushState({}, '', urlWithoutCode);
+
+
+    }
+
+  }, []);
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
