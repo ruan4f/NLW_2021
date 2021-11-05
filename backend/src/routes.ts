58cd440b05e1752db638c6c42b9ee1903d7498ea
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2021-11-04 22:39:28 -0300

    Update

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -8,8 +8,11 @@ import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+
 router.get("/messages/last3", new GetLast3MessagesController().handle);
+
 router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
